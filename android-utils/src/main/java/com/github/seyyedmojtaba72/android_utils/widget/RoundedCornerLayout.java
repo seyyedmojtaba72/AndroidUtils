264b0fa96483676c6e1e4312020a514151f2e041
SeyyedMojtaba Akramzade <akramzade72@gmail.com>
2017-11-10 13:33:36 +0330

    v2.1.4

--- android-utils/src/main/java/com/github/seyyedmojtaba72/android_utils/widget/RoundedCornerLayout.java
+++ android-utils/src/main/java/com/github/seyyedmojtaba72/android_utils/widget/RoundedCornerLayout.java
@@ -2,16 +2,11 @@ package com.github.seyyedmojtaba72.android_utils.widget;
 
 import android.content.Context;
 import android.content.res.TypedArray;
-import android.graphics.Bitmap;
 import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.PorterDuff;
-import android.graphics.PorterDuffXfermode;
+import android.graphics.Path;
 import android.graphics.RectF;
 import android.util.AttributeSet;
-import android.util.DisplayMetrics;
-import android.util.TypedValue;
+import android.util.Log;
 import android.widget.RelativeLayout;
 
 import com.github.seyyedmojtaba72.android_utils.R;
@@ -20,78 +15,66 @@ import com.github.seyyedmojtaba72.android_utils.R;
 /**
  * Created by SeyyedMojtaba on 8/17/2016.
  */
+
 public class RoundedCornerLayout extends RelativeLayout {
-    private static final String TAG = RoundedCornerLayout.class.getSimpleName();
-
-    private static float CORNER_RADIUS = 40.0f;
-
-    private Bitmap maskBitmap;
-    private Paint paint, maskPaint;
-    private float cornerRadius;
+    Path mPath = new Path();
+    float mCornerRadius = 50;
+    private String TAG = RoundedCornerLayout.class.getSimpleName();
 
     public RoundedCornerLayout(Context context) {
         super(context);
-        init(context, null, 0);
+        loadAttributes(context, null);
     }
 
     public RoundedCornerLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
         loadAttributes(context, attrs);
-        init(context, attrs, 0);
     }
 
     public RoundedCornerLayout(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-        init(context, attrs, defStyle);
+        loadAttributes(context, attrs);
     }
+
 
     private void loadAttributes(Context context,
                                 AttributeSet paramAttributeSet) {
         TypedArray localTypedArray = context.obtainStyledAttributes(
                 paramAttributeSet, R.styleable.Widgets);
-        CORNER_RADIUS = localTypedArray.getFloat(R.styleable.Widgets_sau_radius, 40f);
+        mCornerRadius = localTypedArray.getDimension(R.styleable.Widgets_sau_radius, 50f);
         localTypedArray.recycle();
-    }
-
-    private void init(Context context, AttributeSet attrs, int defStyle) {
-        DisplayMetrics metrics = context.getResources().getDisplayMetrics();
-        cornerRadius = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, CORNER_RADIUS, metrics);
-
-        paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-
-        maskPaint = new Paint(Paint.ANTI_ALIAS_FLAG | Paint.FILTER_BITMAP_FLAG);
-        maskPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
-
-        setWillNotDraw(false);
+        setCornerRadius(mCornerRadius);
     }
 
     @Override
     public void draw(Canvas canvas) {
-        Bitmap offscreenBitmap = Bitmap.createBitmap(canvas.getWidth(), canvas.getHeight(), Bitmap.Config.ARGB_8888);
-        Canvas offscreenCanvas = new Canvas(offscreenBitmap);
-
-        super.draw(offscreenCanvas);
-
-        if (maskBitmap == null) {
-            maskBitmap = createMask(canvas.getWidth(), canvas.getHeight());
-        }
-
-        offscreenCanvas.drawBitmap(maskBitmap, 0f, 0f, maskPaint);
-        canvas.drawBitmap(offscreenBitmap, 0f, 0f, paint);
+        canvas.save();
+        canvas.clipPath(mPath);
+        super.draw(canvas);
+        canvas.restore();
     }
 
-    private Bitmap createMask(int width, int height) {
-        Bitmap mask = Bitmap.createBitmap(width, height, Bitmap.Config.ALPHA_8);
-        Canvas canvas = new Canvas(mask);
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        RectF r = new RectF(0, 0, w, h);
+        mPath = new Path();
+        mPath.addRoundRect(r, mCornerRadius, mCornerRadius, android.graphics.Path.Direction.CW);
+        mPath.close();
+    }
 
-        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        paint.setColor(Color.WHITE);
+    public void setCornerRadius(float radius) {
+        Log.e(TAG, "radius: " + radius);
+        mCornerRadius = radius;
+        invalidate();
+    }
 
-        canvas.drawRect(0, 0, width, height, paint);
 
-        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
-        canvas.drawRoundRect(new RectF(0, 0, width, height), cornerRadius, cornerRadius, paint);
-
-        return mask;
+    @Override
+    protected void dispatchDraw(Canvas canvas) {
+        int save = canvas.save();
+        canvas.clipPath(mPath);
+        super.dispatchDraw(canvas);
+        canvas.restoreToCount(save);
     }
 }
