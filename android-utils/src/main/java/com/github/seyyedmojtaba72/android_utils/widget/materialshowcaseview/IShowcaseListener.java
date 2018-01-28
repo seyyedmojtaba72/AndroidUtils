9ab7431176c6148dde97afd80ac668cdf2f44236
SeyyedMojtaba Akramzade <akramzade72@gmail.com>
2018-01-28 12:09:16 +0330

    v2.1.6

---
+++ android-utils/src/main/java/com/github/seyyedmojtaba72/android_utils/widget/materialshowcaseview/IShowcaseListener.java
@@ -0,0 +1,8 @@
+package com.github.seyyedmojtaba72.android_utils.widget.materialshowcaseview;
+
+
+public interface IShowcaseListener {
+    void onShowcaseDisplayed(MaterialShowcaseView showcaseView);
+
+    void onShowcaseDismissed(MaterialShowcaseView showcaseView);
+}
---
+++ android-utils/src/main/java/com/github/seyyedmojtaba72/android_utils/widget/materialshowcaseview/shape/Shape.java
@@ -0,0 +1,35 @@
+package com.github.seyyedmojtaba72.android_utils.widget.materialshowcaseview.shape;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
+import com.telecooleh.artistick.android.util.widget.materialshowcaseview.target.Target;
+
+/**
+ * Specifies a shape of the target (e.g circle, rectangle).
+ * Implementations of this interface will be responsible to draw the shape
+ * at specified center point (x, y).
+ */
+public interface Shape {
+
+    /**
+     * Draw shape on the canvas with the center at (x, y) using Paint object provided.
+     */
+    void draw(Canvas canvas, Paint paint, int x, int y, int padding);
+
+    /**
+     * Get width of the shape.
+     */
+    int getWidth();
+
+    /**
+     * Get height of the shape.
+     */
+    int getHeight();
+
+    /**
+     * Update shape bounds if necessary
+     */
+    void updateTarget(Target target);
+
+}
