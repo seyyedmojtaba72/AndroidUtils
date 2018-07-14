b777e773686c498a92250eda5bf9ea0ed3a0dddf
SeyyedMojtaba Akramzade <akramzade72@gmail.com>
2018-07-14 10:36:45 +0430

    v2.2.4

--- android-utils/src/main/java/com/github/seyyedmojtaba72/android_utils/NotificationUtils.java
+++ android-utils/src/main/java/com/github/seyyedmojtaba72/android_utils/NotificationUtils.java
@@ -1,8 +1,10 @@
 package com.github.seyyedmojtaba72.android_utils;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
@@ -88,7 +90,18 @@ public class NotificationUtils {
     }
 
     public static void showNotification(Context context, Notification notification, int id) {
+        String channelId = "channel-01";
+        String channelName = "Channel Name";
+        int importance = NotificationManager.IMPORTANCE_HIGH;
+
         NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel mChannel = new NotificationChannel(
+                    channelId, channelName, importance);
+            mNotificationManager.createNotificationChannel(mChannel);
+        }
+
         mNotificationManager.notify(id, notification);
     }
 }
